53e5da5a7925c80106348e481bb8380b97176bf1
AngelSolomos <angelsolomos@gmail.com>
2021-08-11 19:48:15 +0300

    second

---
+++ demos/enumerate.py
@@ -0,0 +1,4 @@
+names = ["name_a", "name_b", "name_c", "name_d"]
+
+for index, name in enumerate(names):
+    print(index, name)
--- main.py
+++ main.py
@@ -3,23 +3,30 @@ from read_solution import read_solution, evaluate
 
 
 if __name__ == "__main__":
-    # paths = [
-    #     "car-f-92.stu",
-    #     "car-s-91.stu",
-    #     "ear-f-83.stu",
-    #     "hec-s-92.stu",
-    #     "kfu-s-93.stu",
-    #     "lse-f-91.stu",
-    #     "pur-s-93.stu",
-    #     "rye-s-93.stu",
-    #     "sta-f-83.stu",
-    #     "tre-s-92.stu",
-    #     "uta-s-92.stu",
-    #     "ute-s-92.stu",
-    #     "yor-f-83.stu",
-    # ]
+    paths = [
+        "car-f-92.stu",
+        "car-s-91.stu",
+        "ear-f-83.stu",
+        "hec-s-92.stu",
+        "kfu-s-93.stu",
+        "lse-f-91.stu",
+        "pur-s-93.stu",
+        "rye-s-93.stu",
+        "sta-f-83.stu",
+        "tre-s-92.stu",
+        "uta-s-92.stu",
+        "ute-s-92.stu",
+        "yor-f-83.stu",
+    ]
 
-    graph = read_problem("kfu-s-93.stu")
+    print("choose problem file")
+    for index, path in enumerate(paths):
+        print(index, path)
+    i = input("Enter problem:")
+    problem = paths[int(i)]
+
+    graph = read_problem(problem)
+
     solution = read_solution("kfu-s-93(12.90).sol")
     periods, feasible = evaluate(graph, solution)
     if feasible == True:
